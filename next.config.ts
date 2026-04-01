a16b126351c956a064fcd52c48565f75c07d61f3
shiannona-cloud <shiannona@gmail.com>
2026-04-01 21:09:04 +0800

    添加配置

--- next.config.ts
+++ next.config.ts
@@ -3,3 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
+const nextConfig = {
+  output: 'export',        // 关键！让 Next.js 输出静态文件
+  // 如果你的仓库名叫 shiannona-cloud，加上这行（避免资源路径错误）
+  basePath: '/shiannona-cloud',
+  assetPrefix: '/shiannona-cloud/',
+  images: {
+    unoptimized: true,     // GitHub Pages 不支持 Next.js 图片优化
+  },
+}
